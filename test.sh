9f4333f4eb6548065ec08a36ebaf9cf51bf737e9
XeRh0 <187914292+XeRh0@users.noreply.github.com>
2025-07-02 20:22:33 +0200

    Modify test script

--- test.sh
+++ test.sh
@@ -4,18 +4,22 @@ printf "[Running tests!]\n";
 
 for DIR in tests/*; do
 
-  printf "> Test suite: ${DIR##*/}\n";
+  printf "> Test suite: [${DIR##*/}]\n";
 
   for TEST in $DIR/in/*; do
     printf "Test \"${TEST##*/}\": ";
 
     cat $TEST | ./obsidian-to-quarto.pl > tmp.txt;
-    diff tmp.txt $DIR/ref/${TEST##*/};
+    diff tmp.txt $DIR/out/out_${TEST##*_} #> /dev/null;
 
     if [[ $? -eq 0 ]]; then
-      echo "[GOOD]";
+      echo -e "[\u001b[32mGOOD\u001b[37m]";
     else
-      echo "[FAIL]";
+      echo -e "[\u001b[31mFAIL\u001b[37m]";
     fi
-  done
-done 
+  done 
+done
+
+if [[ -f "tmp.txt" ]]; then
+  rm "tmp.txt";
+fi
