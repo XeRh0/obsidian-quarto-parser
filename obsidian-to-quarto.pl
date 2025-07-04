b52d532aeeb99292da586ebe31178a215b3463fe
XeRh0 <187914292+XeRh0@users.noreply.github.com>
2025-07-04 22:17:41 +0200

    Add input output interface

--- obsidian-to-quarto.pl
+++ obsidian-to-quarto.pl
@@ -13,23 +13,23 @@ use utf8;
 # ------------------------------------------------------------------------------
 
 use constant CALLOUT_REGEX => qr/> \[\!([^]]+)\]([+-]?) (.*)/;
-my $CODEBLOCK_REGEX = qr/^```/;
-my $VERBATIM_REGEX = qr/([^`]*)\`\{([^}]+)\}\ ([^`]*)\`(.*)/;
+use constant CODEBLOCK_REGEX => qr/^```/;
+use constant VERBATIM_REGEX => qr/([^`]*)\`\{([^}]+)\}\ ([^`]*)\`(.*)/;
 
 # ------------------------------------------------------------------------------
 # Subroutines
 # ------------------------------------------------------------------------------
 
 my sub verbatim_parsing_mode {
-  my ($line) = @_;
+  my ($line, $output_file) = @_;
   my $has_newline = ($line =~ "\n");
-  while($line =~ $VERBATIM_REGEX) {
-    print("$1\`$3\`\{\.$2\}"); # reorder matched parts
+  while($line =~ VERBATIM_REGEX) {
+    print($output_file "$1\`$3\`\{\.$2\}"); # reorder matched parts
     $line = $4; # Assign remaining contents to $line, so they can be reused.
   }
-  print($line);
+  print($output_file $line);
   if($has_newline == 1) {
-    print("\n");
+    print($output_file "\n");
   }
   return;
 }
@@ -37,7 +37,7 @@ my sub verbatim_parsing_mode {
 # ------------------------------------------------------------------------------
 
 my sub callout_parsing_mode {
-  my ($line) = @_;
+  my ($line, $input_file, $output_file) = @_;
   my $count = 0;
   my $nesting_level = 0;
   my $codeblock = 0;
@@ -50,57 +50,57 @@ my sub callout_parsing_mode {
     }
     if($line =~ CALLOUT_REGEX && $codeblock == 0) {
       ++$nesting_level;
-      print("\n::: {.$1 title=\"");
+      print($output_file "\n::: {.$1 title=\"");
       $line = $3;
       my $collapse = $2;
-      if($line =~ $VERBATIM_REGEX) {
-        verbatim_parsing_mode($line);
-        print("\"");
+      if($line =~ VERBATIM_REGEX) {
+        verbatim_parsing_mode($line, $output_file);
+        print($output_file "\"");
       } else {
-        print ("$line\"");
+        print ($output_file "$line\"");
       }
       if(!($collapse eq "")) {
-        print(" collapse=", ($2 eq "-")? "true" : "false");
+        print($output_file " collapse=", ($2 eq "-")? "true" : "false");
       }
-      print("}\n");
+      print($output_file "}\n");
       $count = 0;
-      $line = <STDIN>;
+      $line = <$input_file>;
       next;
     } 
     if($count == 0) {
-      print(":::\n");
+      print($output_file ":::\n");
       print($line);
       return;
     }
-    if($line =~ $CODEBLOCK_REGEX) {
+    if($line =~ CODEBLOCK_REGEX) {
       $codeblock = ($codeblock + 1) % 2;
       $count = 0;
-      print($line);
-      $line = <STDIN>;
+      print($output_file $line);
+      $line = <$input_file>;
       next;
     } 
-    if($line =~ $VERBATIM_REGEX) {
-      verbatim_parsing_mode($line);
+    if($line =~ VERBATIM_REGEX) {
+      verbatim_parsing_mode($line, $output_file);
       if(!($line =~ s/\n//)) {
-        print("\n");
+        print($output_file "\n");
       }
-      $line = <STDIN>;
+      $line = <$input_file>;
       next;
     } 
     if($count < $nesting_level) {
       --$nesting_level;
-      print(":::\n");
+      print($output_file ":::\n");
     }
     $count = 0;
-    print($line);
+    print($output_file $line);
     if(!($line =~ s/\n//)) {
-      print("\n");
+      print($output_file "\n");
     }
-    $line = <STDIN>;
+    $line = <$input_file>;
   }
   while($nesting_level > 0) {
     --$nesting_level;
-    print(":::\n");
+    print($output_file ":::\n");
   }
   return;
 }
@@ -111,30 +111,104 @@ my sub callout_parsing_mode {
 # codeblocks, but could be more useful in the future.
 
 my sub codeblock_parsing_mode {
-  my ($line) = @_;
-  while($line = <STDIN>) {
-    print("$line");
-    if($line =~ $CODEBLOCK_REGEX) {
+  my ($line, $input_file, $output_file) = @_;
+  print($output_file $line);
+  while($line = <$input_file>) {
+    print($output_file $line);
+    if($line =~ CODEBLOCK_REGEX) {
       return;
     }
   }
   die("$0: Missing closing codeblock symbol!");
 }
 
+# ------------------------------------------------------------------------------
+
+my sub resolve_flags {
+  my $input_file_name;
+  my $output_file_name;
+
+  for(my $parameter_count = $#ARGV; $parameter_count >= 0;) {
+    my $input = $ARGV[0];
+    if($input eq "-h") {
+      print_help();
+      exit(0);
+    }
+    if($input eq "-i") {
+      if(!(defined $ARGV[1])) {
+        die("$0: Missing input file name!");  
+      }
+      $input_file_name = $ARGV[1];
+      shift @ARGV for 1..2;
+      $parameter_count -= 2;
+      next;
+    }
+    if($input eq "-o") {
+      if(!(defined $ARGV[1])) {
+        die("$0: Missing output file name!");  
+      }
+      $output_file_name = $ARGV[1];
+      shift @ARGV for 1..2;
+      $parameter_count -= 2;
+      next;
+    }
+    if(!defined($input_file_name)) {
+      $input_file_name = $input;
+      shift @ARGV;
+      $parameter_count -= 1;
+      next;
+    }
+    if(!defined($output_file_name)) {
+      $output_file_name = $input;
+      shift @ARGV;
+      $parameter_count -= 1;
+      next;
+    }
+    die("$0: Unknown parameters!");
+  }
+
+  my $input_file;
+  my $output_file;
+
+  if(!defined($input_file_name)) {
+     $input_file = *STDIN; 
+  } else {
+    open($input_file, '<', $input_file_name)
+      or die ("$0: Could not open $input_file_name");
+  }
+
+  if(!defined($output_file_name)) {
+    $output_file = *STDOUT;
+  } else {
+    open($output_file, '>', $output_file_name) 
+      or die ("$0: Could not open $output_file_name");
+  }
+
+  return ($input_file, $output_file);
+}
 
 # ------------------------------------------------------------------------------
 # Main
 # ------------------------------------------------------------------------------
 
-while(my $line = <STDIN>) {
+my ($input_file, $output_file) = resolve_flags();
+
+while(my $line = <$input_file>) {
   if($line =~ CALLOUT_REGEX) {
-    callout_parsing_mode($line);
-  } elsif ($line =~ $CODEBLOCK_REGEX) {
-    print($line);
-    codeblock_parsing_mode($line);
-  } elsif ($line =~ $VERBATIM_REGEX) {
-    verbatim_parsing_mode($line);
+    callout_parsing_mode($line, $input_file, $output_file);
+  } elsif ($line =~ CODEBLOCK_REGEX) {
+    codeblock_parsing_mode($line, $input_file, $output_file);
+  } elsif ($line =~ VERBATIM_REGEX) {
+    verbatim_parsing_mode($line, $output_file);
   } else {
-    print($line);
+    print($output_file $line);
   }
+}
+
+if($input_file ne *STDIN) {
+  close($input_file);
+}
+
+if($output_file ne *STDOUT) {
+  close($output_file);
 }
